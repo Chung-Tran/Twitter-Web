aa56b5414bb5f47ddec34bab01eab9c8f1ad7425
Chung <bitran202@gmail.com>
2024-03-03 19:35:31 +0700

    Init source

--- API/controllers/UserController.js
+++ API/controllers/UserController.js
@@ -3,7 +3,6 @@ const User = require('../model/User');
 const formatResponse = require('../common/ResponseFormat');
 
 const registerUser = asyncHandle(async (req, res) => {
-    console.log("asas")
     const email = req.body.email;
     const username = req.body.username;
     const existData = await User.findOne({ email: email } || { username: username });
@@ -11,8 +10,6 @@ const registerUser = asyncHandle(async (req, res) => {
         return res.status(400).json(formatResponse(null, false, "Register failed!! Try again with other username or email."));
     }
     const newUser = await User.create(req.body);
-    
-    return res.status(200).json(formatResponse(newUser, true, "Register user successfully!"))
-
+    return res.status(200).json(formatResponse(newUser, true, "Register user successfully!"));
 });
-module.exports={registerUser}
+module.exports={registerUser,testController}
--- API/model/Sweet.js
+++ API/model/Sweet.js
@@ -10,7 +10,6 @@ const tweetSchema = new mongoose.Schema({
     shares: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
-
 });
 const Privacys = {
     PRIVACY_PUBLIC: 1001,
