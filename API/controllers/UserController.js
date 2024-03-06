7ca9573d2d41fe5b6a51b51c1f94a9e47aef8dae
Chung <bitran202@gmail.com>
2024-03-07 01:45:44 +0700

    Resetup source

--- API/controllers/UserController.js
+++ API/controllers/UserController.js
@@ -13,37 +13,5 @@ const registerUser = asyncHandle(async (req, res) => {
     return res.status(200).json(formatResponse(newUser, true, "Register user successfully!"));
 });
 
-const TestGithub = asyncHandle(async (req, res) => {
-    const email = req.body.email;
-    const username = req.body.username;
-    const existData = await User.findOne({ email: email } || { username: username });
-    if (existData) {
-        return res.status(400).json(formatResponse(null, false, "Register failed!! Try again with other username or email."));
-    }
-    const newUser = await User.create(req.body);
-    return res.status(200).json(formatResponse(newUser, true, "Register user successfully!"));
-});
 
-const TestGithub2 = asyncHandle(async (req, res) => {
-    const email = req.body.email;
-    const username = req.body.username;
-    const existData = await User.findOne({ email: email } || { username: username });
-    if (existData) {
-        return res.status(400).json(formatResponse(null, false, "Register failed!! Try again with other username or email."));
-    }
-    const newUser = await User.create(req.body);
-    return res.status(200).json(formatResponse(newUser, true, "Register user successfully!"));
-});
-
-const TestGithub3 = asyncHandle(async (req, res) => {
-    const email = req.body.email;
-    const username = req.body.username;
-    const existData = await User.findOne({ email: email } || { username: username });
-    if (existData) {
-        return res.status(400).json(formatResponse(null, false, "Register failed!! Try again with other username or email."));
-    }
-    const newUser = await User.create(req.body);
-    return res.status(200).json(formatResponse(newUser, true, "Register user successfully!"));
-});
-
-module.exports={registerUser,testController}
+module.exports={registerUser}
