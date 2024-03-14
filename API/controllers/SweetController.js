6cf79776a892d0b5f83ce088d1aa484743b0fc40
LoiUTC2 <laihuuloifo4@gmail.com>
2024-03-15 01:03:50 +0700

    Chức năng hiển thị danh sách like, share

--- API/controllers/SweetController.js
+++ API/controllers/SweetController.js
@@ -1,16 +1,16 @@
 const asyncHandle = require('express-async-handler')
 const Sweet = require("../model/Sweet");
-const User = require("../controllers/UserController")
+const User = require("../model/User")
 const formatResponse = require('../common/ResponseFormat');
 const { set } = require('mongoose');
-//const { use } = require('../routes/SweetRoute');
+
 
 const createSweet = asyncHandle(async (req, res) => {
 
     const user_id = req.body.user_id;
     const content = req.body.content;
     const image = req.body.image;
-    const create_At = req.body.create_At;
+    
 
   
     //console.log("ket qua tu params ", req.params.user_id);
@@ -23,15 +23,29 @@ const createSweet = asyncHandle(async (req, res) => {
     });
 
     const data = {
-        likeNumber: createNew.likes.length,
-        content: createNew.content,
-        createAt: createNew.created_at,
+        
+        User: await getDisplayName_By_ID(createNew.user_id),
+        Content: createNew.content,
+        CreateAt: createNew.created_at,
+        likeNumber: createNew.likes.length
         //updateAt: createNew.updated_at
         
     };
 
     return res.status(200).json(formatResponse(data, true, ""));
 });
+
+async function getDisplayName_By_ID(id){
+  const use = await User.findById(id).populate("displayName");
+  if(!use){
+    console.log("Không tìm thấy User!");
+    return null;
+  }
+  else if(use.displayName==null){
+    return use.username;
+  }
+  return use.displayName;
+}
 
 
 async function getID_sweet(userID, create_at) {
@@ -55,23 +69,7 @@ async function getID_sweet(userID, create_at) {
     }
 }
 
-async function getDataByID(user_id, create_At){
-    
-    try {
-        const sweet = await Sweet.findOne({user_id: user_id,created_at: create_At});
-        if(!sweet){
-            console.log("Ko thấy bài viết", sweet);
-            return null;
-        }
-        console.log('Đối tượng Sweet được tìm thấy:', sweet);
-        return sweet;
-    } 
-    catch (error) {
-        console.error("Thất bại trong việc tìm kiếm ID", error.message);
-        return null;
-    }
-    
-}
+
 
 async function get_Sweet_By_Id(id) {
     try {
@@ -117,7 +115,7 @@ const update = asyncHandle(async (req, res) => {
 
     const data = {
        // likeNumber: sweetAfterUpdate.likes.length,
-        UserID: sweetAfterUpdate.user_id,
+        UserID: await getDisplayName_By_ID(user_id),
         Content: sweetAfterUpdate.content,
         CreateAt: sweetAfterUpdate.created_at,
         UpdateAt: sweetAfterUpdate.updated_at
@@ -159,11 +157,7 @@ const deleted = asyncHandle(async (req, res)=> {
 
 
 
-const get = asyncHandle(async (req, res) => {
 
-    console.log("ket qua tu params ", req.query.user_id);
-    
-});
 
 
 const add_User_To_List_Like_Sweet = asyncHandle(async(req, res)=> {
@@ -217,7 +211,87 @@ const add_User_To_List_Share_Sweet = asyncHandle(async(req, res)=> {
 
   
 });
+async function get_List_DisplayName_By_UserID(listUserID){
+  const displayNameS = [];
+  for (const userID of listUserID) {
+    const displayName = await getDisplayName_By_ID(userID);
+    if(displayName){
+      console.log("Tìm thấy UserID và có thể biết được DisplayName");
+      displayNameS.push(displayName);
+      
+    }
+    
+  }
+  return displayNameS;
+}
+
+const get_List_User_To_Like = asyncHandle(async (req, res) => {
+
+  const id_Sweet = req.query.SweetID;
+  const sweet = await Sweet.findById(id_Sweet).populate("likes");
+     
+  try {
+    if(!sweet){
+      console.log("Khong thay bai viet!");
+      
+    }
+  } catch (error) {   
+    console.log("Lỗi khi lấy danh sách like", error);
+  }
+  
+  const sweetPresent = await get_Sweet_By_Id(id_Sweet);
+  console.log("ket qua tu params ", id_Sweet);
+
+  // const List_Userid_ToLike = []; 
+  // for (const id_User of sweetPresent.likes) {
+  //   const dpName = await getDisplayName_By_ID(id_User);
+  //   List_Userid_ToLike.push(dpName);
+  // }
+  // console.log("list User To like:", List_Userid_ToLike);
+
+  //const List_Userid_ToLike = sweetPresent.likes.map(u=>u._id);
+
+  const List_Userid_ToLike = sweetPresent.likes;
+
+  const list_NameUser_ToLike = await get_List_DisplayName_By_UserID(List_Userid_ToLike);
+
+  const data = {
+    List_UserName_ToLike: list_NameUser_ToLike
+  }
+
+  return res.status(200).json(formatResponse(data, true, ""));
+  
+  
+});
+
+const get_List_User_To_Share = asyncHandle(async (req, res) => {
+
+  const id_Sweet = req.query.SweetID;
+  const sweet = await Sweet.findById(id_Sweet).populate("shares");
+     
+  try {
+    if(!sweet){
+      console.log("Khong thay bai viet!");
+      
+    }
+  } catch (error) {
+    console.log("Lỗi khi lấy danh sách like", error);
+  }
+  
+  const sweetPresent = await get_Sweet_By_Id(id_Sweet);
+  console.log("ket qua tu params ", id_Sweet);
+
+  const list_Userid_ToShare = sweetPresent.shares;
+  const list_NameUser_ToShare = await get_List_DisplayName_By_UserID(list_Userid_ToShare);
 
 
+  const data = {
+    List_UserName_ToShare: list_NameUser_ToShare
+  }
 
-module.exports={createSweet, update, deleted, get, add_User_To_List_Like_Sweet, add_User_To_List_Share_Sweet}
+  return res.status(200).json(formatResponse(data, true, ""));
+  
+  
+});
+
+module.exports={createSweet, update, deleted, add_User_To_List_Like_Sweet, add_User_To_List_Share_Sweet, get_List_User_To_Like, get_List_User_To_Share}
--- API/routes/SweetRoute.js
+++ API/routes/SweetRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { createSweet, update, get, deleted, add_User_To_List_Like_Sweet, add_User_To_List_Share_Sweet} = require('../controllers/SweetController');
+const { createSweet, update, get_List_User_To_Like, get_List_User_To_Share, deleted, add_User_To_List_Like_Sweet, add_User_To_List_Share_Sweet} = require('../controllers/SweetController');
 
 router.post('/createSweet', createSweet);
 router.put('/update', update);
 router.delete('/delete', deleted);
 router.post('/addUserLike/:SweetID', add_User_To_List_Like_Sweet);
 router.post('/addUserShare/:SweetID', add_User_To_List_Share_Sweet);
-router.get('/getstatus', get);
+router.get('/getListLike', get_List_User_To_Like);
+router.get('/getListShare', get_List_User_To_Share);
 
 module.exports = router;
