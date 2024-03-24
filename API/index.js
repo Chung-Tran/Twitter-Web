6146f220d5f61344c704256061b11137ada3c544
Chung <bitran202@gmail.com>
2024-03-25 00:27:10 +0700

    commit

--- API/index.js
+++ API/index.js
@@ -23,8 +23,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cors());
 //Connect redis server in docker
- const redisClient = connectRedis();
-redisClient.connect();
+//  const redisClient = connectRedis();
+// redisClient.connect();
 
 //Middleware xác thực người dùng
 app.use('/api/authentication', authenticationRoute);
