bf1c316b6946b3962fa6b764d4969306600ddec3
PhatMinhLe0905 <lephatlmp@gmail.com>
2024-03-28 10:28:52 +0700

    Commit api user

--- API/controllers/AuthController.js
+++ API/controllers/AuthController.js
@@ -3,6 +3,7 @@ const User = require('../model/User');
 const crypto = require('crypto');
 const asyncHandle = require('express-async-handler');
 const {generateToken,generateSessionToken,authenticationToken} = require('../middleware/authMiddleware')
+const formatResponse = require('../common/ResponseFormat');
 
 const loginUser = asyncHandle(async (req, res) => {
     const { email, password } = req.body;
@@ -30,4 +31,32 @@ const loginUser = asyncHandle(async (req, res) => {
         throw new Error("Invalid credential");
     }
 });
+
+// Đăng ký người dùng và tạo token JWT
+const registerUser = asyncHandler(async (req, res) => {
+    const { email, username, password } = req.body;
+
+    try {
+        // Kiểm tra xem email đã tồn tại trong cơ sở dữ liệu chưa
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json(formatResponse(null, false, 'Email đã được sử dụng.'));
+        }
+
+        // Mã hóa mật khẩu
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Tạo một người dùng mới
+        const newUser = await User.create({ email, username, password: hashedPassword });
+
+        // Tạo token JWT cho người dùng mới đăng ký
+        const { refreshToken, accessToken } = generateToken({ userId: newUser._id });
+
+        res.status(200).json(formatResponse({ user: newUser, accessToken, refreshToken }, true, 'Đăng ký người dùng thành công.'));
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).json(formatResponse(null, false, 'Đã xảy ra lỗi trong quá trình đăng ký người dùng.'));
+    }
+});
+
 module.exports={loginUser}
