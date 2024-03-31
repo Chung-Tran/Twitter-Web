046a3ffc4c90dc2a7f69225e4e86c987e6bdcd87
LoiUTC2 <laihuuloifo4@gmail.com>
2024-03-31 22:21:08 +0700

    check image

--- API/routes/CommentRoute.js
+++ API/routes/CommentRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const uploadImage = require('../middleware/UploadImageMiddleware');
+
 const { create_Comment, update_Comment, delete_Comment} = require("../controllers/CommentController");
 
-router.post('/createComment/:SweetID', create_Comment);
-router.put('/updateComment/:CommentID', update_Comment);
+router.post('/createComment/:SweetID', uploadImage.array('image'), create_Comment);
+router.put('/updateComment/:CommentID', uploadImage.array('image'), update_Comment);
 router.delete('/deleteComment/:CommentID', delete_Comment);
 
 module.exports = router;
--- API/routes/ShareRoute.js
+++ API/routes/ShareRoute.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const uploadImage = require('../middleware/UploadImageMiddleware');
+
 const {create_Share, update_Share, delete_Share, 
         add_OR_Delete_User_To_List_Like_Share,
         get_List_User_To_Like,
         get_List_Comment_To_Share,
         } = require("../controllers/ShareController");
 
-router.post("/createShare/:SweetID", create_Share);
-router.put("/updateShare/:ShareID", update_Share);
+router.post("/createShare/:SweetID", uploadImage.array('image'), create_Share);
+router.put("/updateShare/:ShareID", uploadImage.array('image'), update_Share);
 router.delete("/deleteShare/:ShareID", delete_Share);
 
 router.put('/addOrDeleleLike/:ShareID', add_OR_Delete_User_To_List_Like_Share);
